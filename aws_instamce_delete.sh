a4a773aa1140c8442a94121a56efd391b369f7fc
Naren <lnaren532@gmail.com>
2025-05-22 19:01:40 +0530

    roboshop

--- aws_instamce_delete.sh
+++ aws_instamce_delete.sh
@@ -4,26 +4,50 @@
 # Store as a map (name -> id) or paired array
 
 # Get instance ID and Name using jq-like query
-readarray -t instance_info < <(
-  aws ec2 describe-instances \
-    --filters "Name=instance-state-name,Values=running" \
-    --query "Reservations[*].Instances[*].[InstanceId, Tags[?Key=='Name']|[0].Value]" \
-    --output text
-)
+# readarray -t instance_info < <(
+#   aws ec2 describe-instances \
+#     --filters "Name=instance-state-name,Values=running" \
+#     --query "Reservations[*].Instances[*].[InstanceId, Tags[?Key=='Name']|[0].Value]" \
+#     --output text
+# )
 
 # Create associative array (requires Bash 4+)
-declare -A instance_map
+# declare -A instance_map
 
 # Populate map: Name -> InstanceID
-for line in "${instance_info[@]}"; do
-  instance_id=$(echo "$line" | awk '{print $1}')
-  name=$(echo "$line" | awk '{$1=""; print $0}' | sed 's/^ *//')
+# for line in "${instance_info[@]}"; do
+#   instance_id=$(echo "$line" | awk '{print $1}')
+#   name=$(echo "$line" | awk '{$1=""; print $0}' | sed 's/^ *//')
   
-  instance_map["$name"]=$instance_id
-done
+#   instance_map["$name"]=$instance_id
+# done
 
-# Print the instance names and IDs
-echo "Instance Name -> ID map:"
-for name in "${!instance_map[@]}"; do
-  echo "$name -> ${instance_map[$name]}"
+# # Print the instance names and IDs
+# echo "Instance Name -> ID map:"
+# for name in "${!instance_map[@]}"; do
+#   echo "$name -> ${instance_map[$name]}"
+# done
+
+#deleting instances
+instances=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].[InstanceId, Tags[?Key=='Name']|[0].Value]" --output text)
+
+for instance_id in ${instances[@]} 
+do
+    echo "Terminating instance: $instance_id"
+    
+    if [$instace_id != "shell"]
+    then
+        # Terminate the instance
+        aws ec2 terminate-instances --instance-ids "$instance_id" --output table
+
+        # Check if termination was successful
+        if [ $? -eq 0 ] 
+        then
+            echo "Successfully initiated termination of instance: $instance_id"
+        else
+            echo "Failed to terminate instance: $instance_id"
+        fi
+    fi
+   
 done
+ 
